b86f30117e065e5cad131ef822cec6eae7a0d0c5
Paweł Kusiński <KusinskiP@protonmail.com>
2021-05-10 22:56:01 +0200

    Add new message templates

--- client/lib/message.py
+++ client/lib/message.py
@@ -6,3 +6,9 @@ class Messages:
 
     def command_message(self, cmd):
         return {OP: "cmd", ARGS: ["bash", "-c", cmd]}
+
+    def getfile_message(self, path):
+        return {OP: "getfile", ARGS: path}
+
+    def putfile_message(self, path, content):
+        return {OP: "putfile", ARGS: {"path": path, "content": content}}
